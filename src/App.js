d4e9d2a59a541e8b73fd932fc0910fd436bd22be
lucelaya <lucelaya@gmail.com>
2022-09-10 20:14:15 -0300

    PreEntregaReact

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-// import NavBar from './components/NavBar';
-// import ItemListContainer from './components/ItemListContainer';
-import { MyComicsContainer } from './components/MyComicsContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+// import Footer from './components/Footer';
+import ItemDetailContainer from './components/shop/ItemDetailContainer';
+import ItemListContainer from './components/shop/ItemListContainer';
+import ItemFilterSandman from './components/shop/ItemFilterSandman';
+import AppHome from './components/shop/AppHome';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Tienda Reactiva-Celaya Lucas</h1>
-      {/* <NavBar/> */}
-      {/* <ItemListContainer greetings={'Saludos José'}/> */}
-      <MyComicsContainer />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<AppHome />} />
+        <Route path='/shop' element={<ItemListContainer />} />
+        <Route path='/shop/item/:slug' element={<ItemDetailContainer />} />
+        <Route path='/sandman' element={<ItemFilterSandman />} />
+      </Routes>
+      {/* <Footer/> */}
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/shop/AppHome.jsx
@@ -0,0 +1,14 @@
+import '../../App.css';
+import { MyComicsContainer } from '../../components/MyComicsContainer';
+
+function AppHome() {
+
+    return (
+        <div className="App">
+            <h1>Tienda Reactiva-Celaya Lucas</h1>
+            <MyComicsContainer />
+        </div>
+    );
+}
+
+export default AppHome;
